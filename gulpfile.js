bd4862f821c5ad4204bac1346a616a8331fcbed2
Tim Huisman <tim@hoppinger.com>
2015-08-11 19:46:13 +0200

    Initial commit: add WP & Gulp

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+// Plugins
+var gulp         = require('gulp'),
+    requireDir   = require('require-dir');
+
+// Variables
+var root_path = '/public/wp-content/themes/???';
+// Source paths
+var scss_path       = root_path+'/assets/src/scss/**/*.scss',
+    js_path         = root_path+'/assets/src/js/*.js',
+    js_plugins_path = root_path+'/assets/src/js/plugins/*.js',
+    js_admin_path   = root_path+'/assets/src/js/admin/*.js',
+    img_path        = root_path+'/assets/src/img/*';
+
+// Default task
+gulp.task('default', ['clean'], function() {
+  gulp.start('styles', 'scripts', 'scripts-plugins', 'scripts-admin', 'images');
+});
+
+// Watch
+gulp.task('watch', function() {
+  // Watch .scss files
+  gulp.watch(scss_path, ['styles']);
+
+  // Watch .js files
+  gulp.watch(js_path, ['scripts']);
+  gulp.watch(js_plugins_path, ['scripts-plugins']);
+  gulp.watch(js_admin_path, ['scripts-admin']);
+
+  // Watch image files
+  gulp.watch(img_path, ['images']);
+});
+
+
+// Theme Tasks
+requireDir('/tasks/gulp');
---
+++ tasks/gulp/clean.js
@@ -0,0 +1,15 @@
+// Plugins
+var gulp = require('gulp'),
+    del  = require('del');
+
+// Variables
+var root_path = '/public/wp-content/themes/???';
+// Dist paths
+var css_dist_path = root_path+'/assets/dist/css';
+var js_dist_path  = root_path+'/assets/dist/js';
+var img_dist_path = root_path+'/assets/dist/img';
+
+// Clean
+gulp.task('clean', function(cb) {
+  del([css_dist_path, js_dist_path, img_dist_path], cb)
+});

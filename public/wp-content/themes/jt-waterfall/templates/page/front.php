da7e6bb49c68e869732da8243f4b94d67adfc6a9
Tim Huisman <tim@hoppinger.com>
2015-08-13 13:20:57 +0200

    Implement main content

---
+++ public/wp-content/themes/jt-waterfall/templates/page/front.php
@@ -0,0 +1,107 @@
+<div class="page-content container cf">
+  <div class="page__main">
+    <div class="page__title-wrapper title--striped">
+      <h2 class="page__title">Mattis Bibendum Ipsum</h2>
+    </div>
+
+    <div class="page__content">
+      <div class="page__text page__text--main">
+        <p>Curabitur blandit tempus porttitor. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Curabitur blandit tempus porttitor.</p>
+
+        <div class="page__button">
+          <a href="#" class="btn btn--red btn--more">Lees meer</a>
+        </div>
+      </div>
+
+      <div class="page__image page__image--main">
+        <div class="image__inner">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/block-img.png" width="350" height="225" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="page__blocks">
+  <div class="container cf">
+    <div class="block__inner">
+      <div class="page__block">
+        <div class="page__content">
+          <div class="page__text page__text--block">
+            <div class="page__title-wrapper title--striped">
+              <h3 class="page__title color--grey">Mattis Bibendum Ipsum</h3>
+            </div>
+
+            <p>Curabitur blandit tempus porttitor. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Curabitur blandit tempus porttitor.</p>
+
+            <div class="page__button page__button--block">
+              <a href="#" class="btn btn--red btn--more btn--more--block">Lees meer</a>
+            </div>
+          </div>
+
+          <div class="page__image">
+            <div class="image__inner page__image--grey page__image--block">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/block-img.png" width="350" height="225" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="page__block">
+        <div class="page__content">
+          <div class="page__text page__text--block">
+            <div class="page__title-wrapper title--striped">
+              <h3 class="page__title color--grey">Mattis Bibendum Ipsum</h3>
+            </div>
+
+            <p>Curabitur blandit tempus porttitor. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Curabitur blandit tempus porttitor.</p>
+
+            <div class="page__button page__button--block">
+              <a href="#" class="btn btn--red btn--more btn--more--block">Lees meer</a>
+            </div>
+          </div>
+
+          <div class="page__image">
+            <div class="image__inner page__image--grey page__image--block">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/block-img.png" width="350" height="225" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="page__block">
+        <div class="page__content">
+          <div class="page__text page__text--block">
+            <div class="page__title-wrapper title--striped">
+              <h3 class="page__title color--grey">Mattis Bibendum Ipsum</h3>
+            </div>
+
+            <p>Curabitur blandit tempus porttitor. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Curabitur blandit tempus porttitor.</p>
+
+            <div class="page__button page__button--block">
+              <a href="#" class="btn btn--red btn--more btn--more--block">Lees meer</a>
+            </div>
+          </div>
+
+          <div class="page__image">
+            <div class="image__inner page__image--grey page__image--block">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/block-img.png" width="350" height="225" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="page__block--featured">
+        <div class="featured__inner">
+          <div class="featured__content">
+            <div class="featured__title-wrapper">
+              <h3 class="h2 featured__title">Mattis Bibendum Ipsum</h3>
+            </div>
+
+            <p class="feature__text">Curabitur blandit tempus porttitor. Donec sed odio dui. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Curabitur blandit tempus porttitor.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
